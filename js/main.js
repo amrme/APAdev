89db9c0d3423cae9b96a0d81573f81f9b2de9faa
amrme <amr.e.dev@gmail.com>
2015-10-07 15:15:04 +0200

    added members js and styles

--- js/main.js
+++ js/main.js
@@ -40,4 +40,49 @@ $(document).ready(function(){
         $scroll.css({ backgroundPosition: coords });
       }); // end window scroll
    });  // end section function
+
+   var teamMembers = [
+     '<div class="col-md-3 member"><span class="member-name">Mohamed</span><a href="#"><img src="img/tahona.jpg" alt="Mohamed Khaled" class=" img-circle team-img"></a></div>',
+     '<div class="col-md-3 member"><span class="member-name">Mostafa</span><a href="#"><img src="img/mostafa.jpg" alt="Mostafa" class=" img-circle team-img"></a></div>',
+     '<div class="col-md-3 member"><span class="member-name">Samer</span><a href="#"><img src="img/samer.jpg" alt="Samer" class=" img-circle team-img"></a></div>'
+
+ ];
+
+ // generate array of random numbers
+ var randomMember = [];
+ for(var i = 0; i < teamMembers.length; i++) {
+   var tmpMember = Math.floor(Math.random() * teamMembers.length);
+ // if non-rep
+  while(randomMember.indexOf(tmpMember) != -1)
+  {
+    tmpMember = Math.floor(Math.random() * teamMembers.length);
+  }
+  randomMember.push(tmpMember);
+ }
+
+ // console.log(randomMember);
+ // console.log(randomMember.length);
+ // console.log(teamMembers.length);
+
+ // generate members
+  for(var i = 0; i < teamMembers.length; i++)
+  {
+    $(".members").append(teamMembers[randomMember[i]]);
+  }
+
+
+// on hover fade out and display name of memeber
+
+$( ".member" ).hover(function() {
+ $(this).children("a").fadeTo( 150, 0.33 );
+ $(this).find('.member-name').fadeIn(150);
+ // $(this).append("Member");
+ // $( this ).fadeIn( 500 );
+});
+
+$(".member").mouseleave(function() {
+  $(this).children("a").fadeTo( 50, 1 );
+  $(this).find('.member-name').fadeOut(50);
+});
+
 }); // close out script
