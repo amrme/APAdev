99adb4f01642c8cd446f84b3ce85071c1aa0245f
amrme <amr.e.dev@gmail.com>
2015-11-03 14:43:52 +0200

    separated content for team16 js

--- js/main.js
+++ js/main.js
@@ -41,83 +41,14 @@ $(document).ready(function(){
       }); // end window scroll
    });  // end section function
 
-   var team16 = '<div class="col-sm-12 col-md-4 member"><span class="member-name">2016 Team</span><a href="teams/team16.html"><img src="http://i.imgur.com/KOmxkag.jpg" alt="2016 AquaPhoton Academy Team" class="img-circle teams-img"></a></div>';
-   var ceo16 = '<div class="col-md-3 member"><span class="member-name">Abdelhamid<br><span class="label label-warning">CEO</span></span><a href="#"><img src="http://i.imgur.com/Gs44kli.jpg" alt="Abdelhamid Kassem" class=" img-circle team-img"></a></div>';
-   var cto16 = [
-     '<div class="col-md-3 member"><span class="member-name">Mostafa<br><span class="label label-primary">CTO</span></span><a href="#"><img src="http://i.imgur.com/RJoGtLd.jpg" alt="Mostafa" class=" img-circle team-img"></a></div>',
-     '<div class="col-md-3 member"><span class="member-name">Ahmad<br><span class="label label-primary">CTO</span></span><a href="#"><img src="http://i.imgur.com/yOPGkcH.jpg" alt="Ahmad Al-Tawil" class=" img-circle team-img"></a></div>',
-   ];
-   var teamMembers16 = [
-     '<div class="col-md-3 member"><span class="member-name">Mohamed</span><a href="#"><img src="http://i.imgur.com/cwlGcKy.jpg" alt="Mohamed Khaled" class=" img-circle team-img member-image"></a></div>',
-     '<div class="col-md-3 member"><span class="member-name">Samer</span><a href="./members/samer.html"><img src="http://i.imgur.com/7jx52I7.jpg?1" alt="Samer" class=" img-circle team-img"></a></div>',
-     '<div class="col-md-3 member"><span class="member-name">Karim</span><a href="#"><img src="http://i.imgur.com/skPTGGD.jpg" alt="Karim Tarek Farag" class=" img-circle team-img"></a></div>',
-     '<div class="col-md-3 member"><span class="member-name">Fouad</span><a href="#"><img src="http://i.imgur.com/wpJWL7T.jpg" alt="Fouad Safawani" class=" img-circle team-img"></a></div>',
-     '<div class="col-md-3 member"><span class="member-name">Hassen</span><a href="#"><img src="http://i.imgur.com/FCLPknF.jpg" alt="Hassen Mohamed Hassen" class=" img-circle team-img"></a></div>',
-     '<div class="col-md-3 member"><span class="member-name">Omar</span><a href="#"><img src="http://i.imgur.com/hEZ64RG.jpg" alt="Omar Naguib Ibrahim Ibrahim Mostafa" class=" img-circle team-img"></a></div>',
-     '<div class="col-md-3 member"><span class="member-name">Omar</span><a href="#"><img src="http://i.imgur.com/0URiCIU.jpg" alt="Omar Adel Kassem" class=" img-circle team-img"></a></div>',
- ];
 
  // var moreMembers = '<div class="col-md-3"><a href="team.html"><div class="circle circle-solid"><div class="circle-inner"><div class="score-text">View More Members</div></div></div></a></div>';
-function random_member(arrOfMembers) {
-  // generate array of random numbers
-  var randomMember = [];
-  for(var i = 0; i < arrOfMembers.length; i++) {
-    var tmpMember = Math.floor(Math.random() * arrOfMembers.length);
-  // if non-rep
-   while(randomMember.indexOf(tmpMember) != -1)
-   {
-     tmpMember = Math.floor(Math.random() * arrOfMembers.length);
-   }
-   randomMember.push(tmpMember);
-  }
-  return randomMember;
-}
-
 
  // console.log(randomMember);
  // console.log(randomMember.length);
  // console.log(teamMembers16.length);
 
- // append CEO
- $("#team16").append(ceo16);
- // append CTOs
- var randCTO = random_member(cto16);
- for(var i = 0; i < cto16.length; i++)
- {
-   $("#team16").append(cto16[randCTO[i]]);
- }
- // generate members
-  var randMembers = random_member(teamMembers16);
-  for(var i = 0; i < teamMembers16.length; i++)
-  {
-    $("#team16").append(teamMembers16[randMembers[i]]);
-  }
 
-  // append teams to the homepage
-  // for(var i = 0; i < 1; i++)
-  // {
-  //   $(".members-min").append(teamMembers16[randomMember[i]]);
-  // }
-// append teams to the homepage
-$(".members-min").append(team16);
-
-
-// on hover fade out and display name of memeber
-
-$( ".member" ).children("a").hover(function() {
- $(this).fadeTo(150, 0.03 );
- // $(this).html('<div styles="background-color:black;"></div>');
- $(this).parent().find('.member-name').fadeIn(250);
- // $(this).parent().find('.member-image').fadeTo( 150, 0.33 );
-
- // $(this).append("Member");
- // $( this ).fadeIn( 500 );
-});
-
-$(".member").children("a").mouseleave(function() {
-  $(this).fadeTo(20, 1 );
-  $(this).parent().find('.member-name').fadeOut(50);
-});
 
 // $(".player").mb_YTPlayer();
 
