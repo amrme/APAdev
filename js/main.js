a034524e5f3a0913d63934c919f8bb6e8c549f7d
amrme <amr.e.dev@gmail.com>
2015-11-03 08:34:00 +0200

    changed color styles

--- js/main.js
+++ js/main.js
@@ -102,7 +102,7 @@ $(".member").children("a").mouseleave(function() {
   $(this).parent().find('.member-name').fadeOut(50);
 });
 
-$(".player").mb_YTPlayer();
+// $(".player").mb_YTPlayer();
 
 // $("#contact-vid").tubular({videoId: 'jEFz76973lM'});
 // $('body').tubular('jEFz76973lM','contact-vid');
