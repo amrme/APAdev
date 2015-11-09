eedd43263ecb89dcc72ffa1101173d88185c2261
amrme <amr.e.dev@gmail.com>
2015-11-09 19:35:18 +0200

    added teams' thumbnails

--- js/main.js
+++ js/main.js
@@ -52,8 +52,14 @@ $(document).ready(function(){
    });  // end section function
 
    var team16 = '<div class="col-sm-12 col-md-4 member"><span class="member-name">2016 Team</span><a href="teams/team16.html"><img src="http://i.imgur.com/KOmxkag.jpg" alt="2016 AquaPhoton Academy Team" class="img-circle teams-img"></a></div>';
+   var team15 = '<div class="col-sm-12 col-md-4 member"><span class="member-name">2015 Team</span><a href="teams/team15.html"><img src="http://i.imgur.com/xCmX1MR.jpg" alt="2015 AquaPhoton Academy Team" class="img-circle teams-img"></a></div>';
+   var team14 = '<div class="col-sm-12 col-md-4 member"><span class="member-name">2014 Team</span><a href="teams/team14.html"><img src="http://i.imgur.com/5AJ1d2y.jpg" alt="2014 AquaPhoton Academy Team" class="img-circle teams-img"></a></div>';
+   var team13 = '<div class="col-sm-12 col-md-4 member"><span class="member-name">2013 Team</span><a href="teams/team13.html"><img src="http://i.imgur.com/Y1sleM0.jpg?1" alt="2013 AquaPhoton Academy Team" class="img-circle teams-img"></a></div>';
    // append teams to the homepage
    $(".members-min").append(team16);
+   $(".members-min").append(team15);
+   $(".members-min").append(team14);
+   $(".members-min").append(team13);
 
    // on hover fade out and display name of memeber
    $( ".member" ).children("a").hover(function() {
