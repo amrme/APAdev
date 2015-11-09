8fd816192e2e3f6a7ea3459e6a4c31137906efd9
amrme <amr.e.dev@gmail.com>
2015-11-09 21:13:16 +0200

    added memebrs pages

--- js/team16.js
+++ js/team16.js
@@ -1,4 +1,10 @@
-
+var supervisor16 = '<div class="col-md-3 member"><span class="member-name">Prof. Ahmed<br>Naguib</span><a href="#"><img src="http://i.imgur.com/Ko02fE0.jpg" alt="Prof. Ahmed Naguib" class=" img-circle team-img"></a></div>';
+var mentors16 = [
+  '<div class="col-md-3 member"><span class="member-name">Moataz<br>Tarek</span><a href="#"><img src="http://i.imgur.com/RrFv4Jc.jpg" alt="Moataz Tarek" class=" img-circle team-img"></a></div>',
+  '<div class="col-md-3 member"><span class="member-name">Ahmed<br>Saeed</span><a href="#"><img src="http://i.imgur.com/EsNVtoc.jpg" alt="Ahmed Saeed" class=" img-circle team-img"></a></div>',
+  '<div class="col-md-3 member"><span class="member-name">Omar<br>Ibn El Khatab</span><a href="#"><img src="http://i.imgur.com/sjuxYnX.jpg" alt="Omar Ibn El Khatab" class=" img-circle team-img"></a></div>',
+  '<div class="col-md-3 member"><span class="member-name">Mohamed<br>Saad</span><a href="#"><img src="http://i.imgur.com/sZjsGxI.jpg?1" alt="Mohamed Saad" class=" img-circle team-img"></a></div>',
+];
 var ceo16 = '<div class="col-md-3 member"><span class="member-name">Abdelhamid<br><span class="label label-warning">CEO</span></span><a href="#"><img src="http://i.imgur.com/Gs44kli.jpg" alt="Abdelhamid Kassem" class=" img-circle team-img"></a></div>';
 var cto16 = [
   '<div class="col-md-3 member"><span class="member-name">Mostafa<br><span class="label label-primary">CTO</span></span><a href="#"><img src="http://i.imgur.com/RJoGtLd.jpg" alt="Mostafa" class=" img-circle team-img"></a></div>',
@@ -14,7 +20,14 @@ var teamMembers16 = [
   '<div class="col-md-3 member"><span class="member-name">Omar</span><a href="#"><img src="http://i.imgur.com/0URiCIU.jpg" alt="Omar Adel Kassem" class=" img-circle team-img"></a></div>',
 ];
 
-
+// append supervisor
+$("#supervisor").append(supervisor16);
+// append mentors
+var randmentor = random_member(mentors16);
+for(var i = 0; i < mentors16.length; i++)
+{
+  $("#mentors").append(mentors16[randmentor[i]]);
+}
 // append CEO
 $("#team16").append(ceo16);
 // append CTOs
