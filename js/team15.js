12cbcfe481c8665cf86b551a1b9ffda832ad04d2
amrme <amr.e.dev@gmail.com>
2015-11-11 04:02:02 +0200

    added team15 page and null image colors

--- js/team15.js
+++ js/team15.js
@@ -1,9 +1,10 @@
-var supervisor16 = '<div class="col-md-12 member"><span class="member-name">Prof. Ahmed<br>Naguib</span><img src="http://i.imgur.com/Ko02fE0.jpg" alt="Prof. Ahmed Naguib" class=" img-circle team-img"></div>';
+var supervisor16 = '<div class="col-md-12 member"><span class="member-name">Prof. Mohamed<br>Elhabrook</span><img src="" alt="" class=" img-circle team-img" style="background-color:white"></div>';
 var mentors16 = [
   '<div class="col-md-3 member"><span class="member-name">Moataz<br>Tarek</span><img src="http://i.imgur.com/RrFv4Jc.jpg" alt="Moataz Tarek" class=" img-circle team-img"></div>',
   '<div class="col-md-3 member"><span class="member-name">Ahmed<br>Saeed</span><img src="http://i.imgur.com/EsNVtoc.jpg" alt="Ahmed Saeed" class=" img-circle team-img"></div>',
   '<div class="col-md-3 member"><span class="member-name">Omar<br>Ibn El Khatab</span><img src="http://i.imgur.com/sjuxYnX.jpg" alt="Omar Ibn El Khatab" class=" img-circle team-img"></div>',
   '<div class="col-md-3 member"><span class="member-name">Mohamed<br>Saad</span><img src="http://i.imgur.com/sZjsGxI.jpg?1" alt="Mohamed Saad" class=" img-circle team-img"></div>',
+  '<div class="col-md-3 member"><span class="member-name">Ramy<br>Abolela</span><img src="" alt="" class=" img-circle team-img" style="background-color:white;"></div>',
 ];
 var ceo16 = '<div class="col-md-3 member"><span class="member-name">Abdelhamid<br><span class="label label-warning">CEO</span></span><img src="http://i.imgur.com/Gs44kli.jpg" alt="Abdelhamid Kassem" class=" img-circle team-img"></div>';
 var cto16 = [
@@ -12,12 +13,8 @@ var cto16 = [
 ];
 var teamMembers16 = [
   '<div class="col-md-3 member"><span class="member-name">Mohamed</span><img src="http://i.imgur.com/cwlGcKy.jpg" alt="Mohamed Khaled" class=" img-circle team-img member-image"></div>',
-  '<div class="col-md-3 member"><span class="member-name">Samer</span><img src="http://i.imgur.com/7jx52I7.jpg?1" alt="Samer" class=" img-circle team-img"></div>',
   '<div class="col-md-3 member"><span class="member-name">Karim</span><img src="http://i.imgur.com/skPTGGD.jpg" alt="Karim Tarek Farag" class=" img-circle team-img"></div>',
-  '<div class="col-md-3 member"><span class="member-name">Fouad</span><img src="http://i.imgur.com/wpJWL7T.jpg" alt="Fouad Safawani" class=" img-circle team-img"></div>',
   '<div class="col-md-3 member"><span class="member-name">Hassen</span><img src="http://i.imgur.com/FCLPknF.jpg" alt="Hassen Mohamed Hassen" class=" img-circle team-img"></div>',
-  '<div class="col-md-3 member"><span class="member-name">Omar</span><img src="http://i.imgur.com/hEZ64RG.jpg" alt="Omar Naguib Ibrahim Ibrahim Mostafa" class=" img-circle team-img"></div>',
-  '<div class="col-md-3 member"><span class="member-name">Omar</span><img src="http://i.imgur.com/0URiCIU.jpg" alt="Omar Adel Kassem" class=" img-circle team-img"></div>',
 ];
 
 // append supervisor
